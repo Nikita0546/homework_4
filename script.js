488d78af5f7e7c3e15350af3e4d48260b61e1ba8
Nikita0546 <116941923+Nikita0546@users.noreply.github.com>
2022-12-20 19:10:29 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,29 @@
+const student = "Кривохижин Никита Андреевич"; // Очевидно, что здесь ваши личные Фамилия, Имя и Отчество
+
+document.getElementById("student").innerHTML = student;
+
+// Отсюда и ниже идет ваш код решения домашнего задания
+// ...
+
+const error_chance = 0.2;
+
+function throwRandomError(chance) {
+    let max_chance = 1.0;
+    let min_chance = 0.0;
+    if ((chance < min_chance) || (chance > max_chance)) {
+    }
+    
+    random_number = Math.random(max_chance).toFixed(1);
+    console.log(`Случайное число: ${random_number}`);
+    if (random_number <= chance) {
+        throw new Error('Ой, что-то сломалось');
+    }
+}
+
+
+try {
+    throwRandomError(error_chance);
+    console.log('Всё в порядке, приложение работает в штатном режиме');
+} catch (e) {
+    console.log(`Произошла ошибка, пожалуйста перезагрузите страницу`);
+}
